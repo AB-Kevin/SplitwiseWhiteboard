06d13702cd9478c6953433c0261c8fcdcf8030f3
AB-Kevin <kevin@anabaptistbrotherhood.org>
2026-08-29 22:11:41 -0400

    Release v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kevin.splitwisewhiteboard"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10001
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
